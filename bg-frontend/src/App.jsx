03bc892f4180fd990f990bfc93a8b5a3660dea94
VinodHadmode <vinodhadmode@gmail.com>
2025-01-20 14:38:11 +0530

    Done with pages routing

--- bg-frontend/src/App.jsx
+++ bg-frontend/src/App.jsx
@@ -1,10 +1,34 @@
+import {Route,Routes} from "react-router-dom"
 import './App.css'
+import Home from "./pages/Home"
+import Clothes from "./pages/Clothes"
+import Electronics from "./pages/Electronics"
+import Furniture from "./pages/Furniture"
+import Toys from "./pages/Toys"
+import Cart from "./pages/Cart"
+import MyAccount from "./pages/MyAccount"
+import MyOrders from "./pages/MyOrders"
+import PageNotFound from "./pages/PageNotFound"
 
 function App() {
 
   return (
     <>
-      <h1 className='border'>BG-frontend Assignment</h1>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/clothes" element={<Clothes/>}/>
+        <Route path="/electronics" element={<Electronics/>}/>
+        <Route path="/furniture" element={<Furniture/>}/>
+        <Route path="/toys" element={<Toys/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/my-account" element={<MyAccount/>}/>
+        <Route path="/my-orders" element={<MyOrders/>}/>
+
+        <Route path="*" element={<PageNotFound/>}/>
+
+      </Routes>
+    </div>
     </>
   )
 }
